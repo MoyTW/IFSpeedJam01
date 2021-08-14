349a915539cee60de04dba0843f20d7d61307d21
MoyTW <moytravis@gmail.com>
2021-08-14 02:38:44 -0700

    gah cside and itch io dont get along

--- export.sh
+++ export.sh
@@ -4,4 +4,5 @@ rm IFSpeedJam01.zip
 rm index.html
 mv IFSpeedJam01.html index.html
 sed -i "s/window.storeName = null;/window.storeName = \"MORGAN_CARLO_AND_MOUSE\";/g" index.html
+sed -i "s/parent && parent.cside/parent/g" index.html
 zip IFSpeedJam01.zip index.html campusmap.png
